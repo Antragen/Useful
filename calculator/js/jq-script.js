32231ff94c8eceb2751d23bbba9754adf4282b1c
Antragen <werty1251@gmail.com>
2017-03-07 17:05:27 +0300

    07.03.2017

--- calculator/js/jq-script.js
+++ calculator/js/jq-script.js
@@ -3,12 +3,14 @@ $(function() {
 		$edition = $( 'select#card_edition' ).val();
 		$paper = $( 'select#card_paper' ).val();
 		$color = $( 'select#card_color' ).val();
-		$paper_ratio = $( 'select#card_paper option:selected' );
+		$print_file = $( 'select#card_color option:selected' ).attr( 'data-print-file' );
+		$paper_ratio = $( 'select#card_paper option:selected' ).attr( 'data-paper-ratio' );
 		$color_format = $( 'select#card_color option:selected' ).attr( 'data-color' );
+		$print = $( 'select#card_color option:selected' ).attr( 'data-print' );
 		$division = 30;
 		$final_edition = $edition / $division;
-		$price = $final_edition * $paper_ratio;
-
+		$price = $final_edition * $paper_ratio + parseInt( $print_file ) + $print;
+		document.getElementById('final_price').innerHTML = $final_edition;
 		if ( $color_format == 1 ) {
 			$( '.card_images img' ).hide();
 			$( '#card_1_0' ).show();
